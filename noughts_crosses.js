faa454bafc5e89f417f291dc0675f85a10d7e8b9
JakeWalshe <57761720+JakeWalshe@users.noreply.github.com>
2020-01-09 14:36:34 +0000

    Add files via upload

---
+++ noughts_crosses.js
@@ -0,0 +1,30 @@
+
+//JavaScript for the Noughts and crosses game
+console.log("Tic Tac Toe Opened")
+
+var cells = document.querySelectorAll("td")
+var clear = document.querySelector("#clear")
+
+clear.addEventListener("click", clearBoard)
+
+function changeMarker() {
+  if (this.textContent ==="") {
+    this.textContent = "X"
+  }
+  else if (this.textContent ==="X") {
+    this.textContent = "O"
+  }
+  else if (this.textContent ==="O") {
+    this.textContent = ""
+  }
+}
+
+function clearBoard() {
+  for (var i = 0; i < cells.length; i++) {
+  cells[i].textContent = ""
+  }
+}
+
+for (var i = 0; i < cells.length; i++) {
+ cells[i].addEventListener("click", changeMarker)
+}
